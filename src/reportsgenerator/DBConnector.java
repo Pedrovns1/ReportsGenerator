908064ffe2f94ebdd2fdbdccc1e78ae51076080e
Pedro Vinicius <peuvini.94@gmail.com>
2024-03-23 13:38:59 +0000

    DB Connector
    Here, I created three strings to hold my sensitive data, then I developed a public class to load the properties from my config.properties file using FileInputStream, as it contains the login information for my database. This approach avoids the bad practice of leaving login information in the code. The process is enclosed within a try-catch block, which ensures the stream is closed after execution. My config.properties file is not on my GitHub because I used .gitignore, as the information is private, so I will upload the file with the assignment submission.

---
+++ src/reportsgenerator/DBConnector.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package reportsgenerator;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ *
+ * @author peuvi
+ */
+public class DBConnector {
+
+    private String dbUrl;
+    private String user;
+    private String password;
+
+    public DBConnector() {
+        // Load properties
+        Properties props = new Properties();
+        try ( FileInputStream input = new FileInputStream("config.properties")) {
+            props.load(input);
+            this.dbUrl = props.getProperty("db.url");
+            this.user = props.getProperty("db.user");
+            this.password = props.getProperty("db.password");
+
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+
+}
