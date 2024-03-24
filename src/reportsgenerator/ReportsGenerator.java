cb236f76e2fa87794359ec8fa1faab96c442ebf3
Pedro Vinicius <peuvini.94@gmail.com>
2024-03-24 14:06:56 +0000

    Main menu implemantation
    Here, I began implementing the main menu and also implemented the variable ReportType that I will use on my method to export the reports. I used a boolean variable to control my loop. Inside the while loop, I added an integer called  'option' that will store the user's input using the scanner. Depending on the user's option, one case from the switch options will be selected. In my switch options, I created a case that will change my boolean variable to false, which will cause the loop to break. Additionally, I created a class called `MainMenu` that will be used to display the options that the user can choose from inside the menu.

--- src/reportsgenerator/ReportsGenerator.java
+++ src/reportsgenerator/ReportsGenerator.java
@@ -19,6 +19,42 @@ public class ReportsGenerator {
     public static void main(String[] args) {
         Connection conn = ReportManager.connect(); // Establish the connection using the ReportManager
         Scanner scanner = new Scanner(System.in);
+        StringBuilder reportData = new StringBuilder();
+        boolean running = true;
+
+        while (running) {
+            MainMenu(); // Shows the main menu
+            int option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    generateReport(conn, ReportType.COURSE, reportData);
+                    break;
+                case 2:
+                    generateReport(conn, ReportType.STUDENT, reportData);
+                    break;
+                case 3:
+                    generateReport(conn, ReportType.LECTURER, reportData);
+                    break;
+                case 4:
+                    System.out.println("Exiting program.");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+        
+        scanner.close();
+    }
+    
+    private static void MainMenu() {
+        System.out.println("\nChoose the report you want to generate:");
+        System.out.println("1. Course Report");
+        System.out.println("2. Student Report");
+        System.out.println("3. Lecturer Report");
+        System.out.println("4. Exit");
+        System.out.print("Enter option: ");
     }
     
 }
