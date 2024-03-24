feddd2790db886a42b6248c4ac11554b10100222
Pedro Vinicius <peuvini.94@gmail.com>
2024-03-24 18:34:57 +0000

    Update SQL query
    I added the function USE to use my schema IEE that contains all the tables.

--- src/reportsgenerator/ReportManager.java
+++ src/reportsgenerator/ReportManager.java
@@ -28,6 +28,7 @@ public class ReportManager {
     }
     public static void generateCourseReport(Connection conn, StringBuilder reportData) {
         String sql = 
+            "USE IEE; " +    
             "SELECT c.Name AS ModuleName, c.Program, COUNT(e.StudentID) AS StudentCount, " +
             "l.Name AS LecturerName, COALESCE(c.Room, 'online') AS Room " +
             "FROM Courses c " +
@@ -52,6 +53,7 @@ public class ReportManager {
     
     public static void generateStudentReport(Connection conn, StringBuilder reportData) {
         String sql = 
+            "USE IEE; " +    
             "SELECT s.Name AS StudentName, s.StudentID, s.Program, " +
             "GROUP_CONCAT(DISTINCT CASE WHEN g.Status = 'In Progress' THEN c.Name END) AS EnrolledModules, " +
             "GROUP_CONCAT(DISTINCT CASE WHEN g.Status = 'Passed' THEN CONCAT(c.Name, ' (Grade: ', g.Score, ')') END) AS CompletedModules, " +
@@ -80,6 +82,7 @@ public class ReportManager {
     
     public static void generateLecturerReport(Connection conn, StringBuilder reportData) {
         String sql = 
+            "USE IEE; " +    
             "SELECT l.Name AS LecturerName, l.Role, c.Name AS ModuleName, COUNT(e.StudentID) AS StudentCount, " +
             "GROUP_CONCAT(DISTINCT c.Type) AS CourseTypes " +
             "FROM Lecturers l " +
