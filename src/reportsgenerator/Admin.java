3cee9ab7c68bbed439df66aeb4257a1a85cd5303
Pedro Vinicius <peuvini.94@gmail.com>
2024-03-25 17:31:23 +0000

    UserModify Method
    The modifyUser method first checks if the username entered by the admin in the menu, exists in the system. If it doesn't, the modification cannot proceed, and a message indicating that the user doesn't exist will be displayed. Additionally, another check is performed to verify if the new username is already in the system. In that case, it will request the selection of a unique username.
    If both checks pass, the function .remove() will eliminate the 'oldUsername' from the map to avoid issues when updating the map, especially if a username is being updated. Then, using a setter, it will update the system with the new username and password, and if the role is changing, the method will instantiate a new user object of the appropriate class based on 'newRole'. This ensures that a new role is assigned to the new user. After all modifications are completed, .put() will re-add the user and their information back to the map.

--- src/reportsgenerator/Admin.java
+++ src/reportsgenerator/Admin.java
@@ -38,5 +38,38 @@ public class Admin extends User {
         }
         System.out.println("User added successfully: " + username);
     }
+    public void modifyUser(Map<String, User> users, String oldUsername, String newUsername, String newPassword, UserRole newRole) {
+    if (!users.containsKey(oldUsername)) {
+        System.out.println("User does not exist on the system.");
+        return;
+    }
+    // Check if new username is already taken by another user
+    if (!oldUsername.equals(newUsername) && users.containsKey(newUsername)) {
+        System.out.println("New username already exists on the system. Please choose a unique username.");
+        return;
+    }  
+    // Remove the old user entry
+    User oldUser = users.remove(oldUsername);
 
+    // Update the user details
+    oldUser.setUsername(newUsername);
+    oldUser.setPassword(newPassword);
+     
+    if (oldUser.getRole() != newRole) {
+        switch (newRole) {
+            case ADMIN:
+                oldUser = new Admin(newUsername, newPassword);
+                break;
+            case OFFICE:
+                oldUser = new Office(newUsername, newPassword);
+                break;
+            case LECTURER:
+                oldUser = new Lecturer(newUsername, newPassword);
+                break;
+        }
+    }
+    // Add the updated user back into the map
+    users.put(newUsername, oldUser);
+    System.out.println("User's details updated successfully: " + newUsername);
+  }
 }
