5aa50e520c4b52d9267ecf5d15b8fd735f5a8676
Pedro Vinicius <peuvini.94@gmail.com>
2024-03-25 14:59:03 +0000

    UserAuthenticator Class
    For my 'UserAuthenticator' class, I am using a 'Map' as a '<key-value>' pair structure to find the user associated with a given username. Then, a scanner is used to obtain the user's input. If the information matches the user data in the map, it will return the user,  otherwise, it won't authenticate the user.

--- src/reportsgenerator/UserAuthenticator.java
+++ src/reportsgenerator/UserAuthenticator.java
@@ -4,10 +4,25 @@
  */
 package reportsgenerator;
 
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  *
  * @author peuvi
  */
 public class UserAuthenticator {
-    
+    public static User authenticateUser(Map<String, User> users) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter username: ");
+        String username = scanner.next();
+        System.out.print("Enter password: ");
+        String password = scanner.next();
+
+        User user = users.get(username);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null; // Authentication failed
+    }
 }
