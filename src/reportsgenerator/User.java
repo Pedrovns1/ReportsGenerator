d5a8c32dcb994dc504f058f7b364c63b8863aa09
Pedro Vinicius <peuvini.94@gmail.com>
2024-03-25 12:53:35 +0000

    User Class
    For the User class, I chose to use an abstract class so that other classes can utilize its information instead of directly creating a user object in other classes. Then, I protected the string information along with the UserRole so that it can only be accessed inside the User class,  protecting the username and password from being accessed from outside of my user class. I created a constructor so that whenever a class uses the User information, it will initialize with a username, password, and role.
    Additionally, I added getter and setter methods to retrieve user information and also to set and change new users in the system.
    The generateReport method in this class ensures that depending on which role is using the system, it will have its own specifications for generating the report. Because it is abstract, it will adapt to different roles.
    The 'changeCredentials' method will use setter methods to update new usernames and passwords in the system.

--- src/reportsgenerator/User.java
+++ src/reportsgenerator/User.java
@@ -8,6 +8,42 @@ package reportsgenerator;
  *
  * @author peuvi
  */
-public class User {
-    
+public abstract class User {
+    protected String username;
+    protected String password;
+    protected UserRole role;
+
+    public User(String username, String password, UserRole role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public abstract void generateReport();
+
+    public void changeCredentials(String newUsername, String newPassword) {
+        setUsername(newUsername);
+        setPassword(newPassword);
+        System.out.println("Credentials changed for user: " + newUsername);
+    }
 }
