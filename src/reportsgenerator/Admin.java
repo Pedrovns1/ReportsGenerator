1f921a3eacf109de5f20d01d7d5515ea98a294d3
Pedro Vinicius <peuvini.94@gmail.com>
2024-03-25 16:14:19 +0000

    Admin Class
    I started by applying the same logic implemented in the Office and Lecturer classes and also, to prevent the admin from generating reports, I implemented the necessary logic in my main code. Additionally, I implemented a method that allows the admin to add users to the map <String, User> only if the user is not already in the system. The switch statement then executes the appropriate code based on the role.

--- src/reportsgenerator/Admin.java
+++ src/reportsgenerator/Admin.java
@@ -4,10 +4,39 @@
  */
 package reportsgenerator;
 
+import java.util.Map;
+
 /**
  *
  * @author peuvi
  */
-public class Admin {
-    
+public class Admin extends User {
+    public Admin(String username, String password) {
+        super(username, password, UserRole.ADMIN);
+    }
+
+    @Override
+    public void generateReport() {
+        System.out.println("Admins cannot generate reports.");
+    }
+
+    public void addUser(Map<String, User> users, String username, String password, UserRole role) {
+        if (users.containsKey(username)) {
+            System.out.println("User already exists.");
+            return;
+        }
+        switch (role) {
+            case ADMIN:
+                users.put(username, new Admin(username, password));
+                break;
+            case OFFICE:
+                users.put(username, new Office(username, password));
+                break;
+            case LECTURER:
+                users.put(username, new Lecturer(username, password));
+                break;
+        }
+        System.out.println("User added successfully: " + username);
+    }
+
 }
